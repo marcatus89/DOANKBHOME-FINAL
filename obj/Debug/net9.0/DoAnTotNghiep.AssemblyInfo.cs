8127d1a3d16563c41ad4b6a1d0e6f6375e86e215
thien <marcatus89@gmail.com>
2025-10-06 19:17:47 +0700

    update3

--- obj/Debug/net9.0/DoAnTotNghiep.AssemblyInfo.cs
+++ obj/Debug/net9.0/DoAnTotNghiep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnTotNghiep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8569b626341fae9bd01999472611a809b499f084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a750ed34009b72b99eebd01b9dd25ad441c32d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnTotNghiep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnTotNghiep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
